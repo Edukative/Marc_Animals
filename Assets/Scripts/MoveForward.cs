cbc9bd6d0dbcbc00429a87bc1daab400a4e460c5
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-03 16:45:21 +0200

    Added Marc's 2nd project

---
+++ Assets/Scripts/MoveForward.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed;
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // Destroy if is too far
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+            print("it works");
+        }
+
+        // Destroy if it alredy passed beyond the player movement
+        if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+            print("die animal");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private float horizontalInput;
+    private float xRange = 10.0f; // Range limit that the player can move
+    public GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space)) // if you press "space"
+        {
+            // Shoot a food prefab
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
+        }
+        
+        // Move left/right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+
+        // Limit the movement of the player
+        if (transform.position.x < -xRange) // too far to the left
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xRange) //too far to the right
+        {
+            // Capped at the same position
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+    }
+}
